69203cce66535b6fa74ae2aa8c2484be5dbb71e2
rtroe-xps <rob.tom.roe@gmail.com>
2023-03-25 21:21:54 -0700

    Initial upload of files

---
+++ ci/set_version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+echo "Set Assembly Version"
+VERSION=1.1.0
+PROJECT_DIR="./src"
+CSPROJECT_DLL="VerticesEngine.Physics.Bepu.csproj"
+#DOX_PATH="./docs/Doxyfile"
+
+
+echo "Setting Assembly and Nuget Pacakge versions to: "$VERSION.$@
+
+# Set DLL Version
+sed -E -i "s/1.0.0/$VERSION.$@/g" $PROJECT_DIR/$CSPROJECT_DLL
+
+# Set Doc Version
+#sed -E -i "s/v1.0.0/v$VERSION.$@/g" ${DOX_PATH}
+
+# Set Assembly Version
+#sed -E -i "s/$VERSION.999/$VERSION.$@/g" "${PROJECT_DIR}/Properties/AssemblyInfo.cs"
+
+
+echo "Done"
---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+//using System.Reflection;
+//using System.Runtime.CompilerServices;
+//using System.Runtime.InteropServices;
+
+//#if VRTC_PLTFRM_iOS || VRTC_PLTFRM_DROID 
+//using Android.App;
+
+//#else
+
+//// General Information about an assembly is controlled through the following 
+//// set of attributes. Change these attribute values to modify the information
+//// associated with an assembly.
+//[assembly: AssemblyTitle("Vertices.Physics.Farseer")]
+//[assembly: AssemblyProduct("Vertices.Physics.Farseer")]
+//[assembly: AssemblyDescription("Vertices Engine Build of the Farseer Physics Library.")]
+//[assembly: AssemblyCompany("")]
+//[assembly: AssemblyCopyright("")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
+
+//// Setting ComVisible to false makes the types in this assembly not visible 
+//// to COM components.  If you need to access a type in this assembly from 
+//// COM, set the ComVisible attribute to true on that type. Only Windows
+//// assemblies support COM.
+//[assembly: ComVisible(false)]
+
+//// On Windows, the following GUID is for the ID of the typelib if this
+//// project is exposed to COM. On other platforms, it unique identifies the
+//// title storage container when deploying this assembly to the device.
+//[assembly: Guid("b14f0b06-95f7-4957-830a-2db6a1810249")]
+
+//// Version information for an assembly consists of the following four values:
+////
+////      Major Version
+////      Minor Version 
+////      Build Number
+////      Revision
+////
+//[assembly: AssemblyVersion("1.1.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
+
+//#endif
